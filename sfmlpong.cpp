bd029e46a28f4aa2f71f0bca533b3d7790f6e4f6
Christopher Rodriguez <cdr255@gmail.com>
2014-02-23 01:00:20 -0500

    Added Ball Class, changed movement to float-base.

--- sfmlpong.cpp
+++ sfmlpong.cpp
@@ -2,11 +2,15 @@
 
 int main()
 {
-  Paddle Player1(20, 100, 0, 20, 200, 400);
-  Paddle Player2(760, 100, 0, 20, 200, 400);
+  Paddle player1(20, 100, 0, 20, 200, 400);
+  Paddle player2(760, 100, 0, 20, 200, 400);
+  Ball ball(0.25);
 
-  Player1.print();
-  Player2.print();
+  player1.print();
+  player2.print();
+  ball.print();
+  ball.serve();
+  ball.print();
 
   return EXIT_SUCCESS;
 }
--- sfmlpong.hpp
+++ sfmlpong.hpp
@@ -5,9 +5,9 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 // Global Constant Definition
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-#define WINDOW_DEPTH 32
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define SCREEN_DEPTH 32
 #define PADDLE_WIDTH 20
 #define PADDLE_HEIGHT 200
 #define PADDLE_SPEED 400
@@ -15,19 +15,19 @@
 
 class Paddle
 {
-  int x;
-  int y;
+  float x;
+  float y;
   int score;
   int width;
   int height;
   int speed;
 public:
-  Paddle(int, int, int, int, int, int);
+  Paddle(float, float, int, int, int, int);
   void print() const;
-  void move(int, int);
+  void move(float, float);
 };
 
-Paddle::Paddle(int setx, int sety, int setscore, int setwidth, int setheight, int setspeed)
+Paddle::Paddle(float setx, float sety, int setscore, int setwidth, int setheight, int setspeed)
 {
   x = setx;
   y = sety;
@@ -38,19 +38,97 @@ Paddle::Paddle(int setx, int sety, int setscore, int setwidth, int setheight, in
 }
 
 void Paddle::print() const
-{
+{ // Const keeps it from modifying variables in the class. It will only read them.
   std::cout << x << " " << y << " " << score << " " << width << " " << height << " "  << speed << "\n";
 }
  
-void Paddle::move(int dx, int dy)
+void Paddle::move(float dx, float dy)
 {
   x = x + dx;
   y = y + dy;
 }
 
+class Ball
+{
+  float x;
+  float y;
+  float xvel;
+  float yvel;
+  int width;
+  int height;
+  float difficulty;
+  int max;
+public:
+  Ball(float);
+  void serve();
+  void move();
+  void print() const;
+};
 
+Ball::Ball(float diff)
+{
+  x = (SCREEN_WIDTH - BALL_SIDE) / 2;
+  y = (SCREEN_HEIGHT - BALL_SIDE) / 2;
+  xvel = 0;
+  yvel = 0;
+  width = BALL_SIDE;
+  height = BALL_SIDE;
+  difficulty = diff;
+  max = 6;
+}
 
+void Ball::serve()
+{
+  int random = sf::Randomizer::Random(0, 7);
+  switch (random)
+    {
+    case 0:
+      xvel = 1;
+      yvel = 1;
+      break;
+    case 1:
+      xvel = 1;
+      yvel = 0.5;
+      break;
+    case 2:
+      xvel = 1;
+      yvel = -0.5;
+      break;
+    case 3:
+      xvel = 1;
+      yvel = -1;
+      break;
+    case 4:
+      xvel = -1;
+      yvel = 1;
+      break;
+    case 5:
+      xvel = -1;
+      yvel = 0.5;
+      break;
+    case 6:
+      xvel = -1;
+      yvel = -0.5;
+      break;
+    case 7:
+      xvel = -1;
+      yvel = -1;
+      break;
+    default:
+      xvel = 1;
+      yvel = 1;
+    }
+}
 
+void Ball::move()
+{
+  std::cout << "Not Implemented Yet.\n";
+}
+      
+void Ball::print() const
+{
+  std::cout << x << " " << y << " " << xvel << " " << yvel << " " << width << " " << height << " " << difficulty << " " << max << "\n";
+}
 
 
 
